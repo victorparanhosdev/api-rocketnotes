9963fa200aa70330c48984189c045d338ab6e68d
Victor Paranhos <victor_paranhos@hotmail.com>
2023-02-11 23:13:33 -0300

    Criação do Banco de Dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("express-async-errors")
 
+const database = require("./database/sqlite")
+
 const AppError = require("./utils/AppError")
 
 const express = require("express")
@@ -8,11 +10,17 @@ const routes = require("./routes")
 
 const app = express();
 app.use(express.json());
+
+
+
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
 
     if(error instanceof AppError){
+    
         return response.status(error.statusCode).json({
             status: "error",
             message: error.message,
