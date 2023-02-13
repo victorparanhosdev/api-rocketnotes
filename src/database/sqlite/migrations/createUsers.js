dc6380d507509b723e514aff17fa79695dac2d11
Victor Paranhos <victor_paranhos@hotmail.com>
2023-02-13 09:15:37 -0300

    add migrations

--- src/database/sqlite/migrations/createUsers.js
+++ src/database/sqlite/migrations/createUsers.js
@@ -1,10 +1,11 @@
 const createUsers = `
-CREATE TABLE cadastro (
+  CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   name VARCHAR, 
   email VARCHAR, 
-  password VARCHAR, 
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  avatar VARCHAR, 
+  created_at DATE,
+  updated_at DATE
 )
 `
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("express-async-errors")
 
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const AppError = require("./utils/AppError")
 
@@ -8,14 +8,14 @@ const express = require("express")
 
 const routes = require("./routes")
 
+migrationsRun()
+
 const app = express();
 app.use(express.json());
 
-
-
 app.use(routes)
 
-database()
+
 
 app.use((error, request, response, next) => {
 
