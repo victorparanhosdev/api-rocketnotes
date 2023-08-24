f9fe0f3f1c4eb5a1ca85a50c5fe8019d2751bc86
Victor Paranhos <victor_paranhos@hotmail.com>
2023-08-24 11:07:00 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -5,11 +5,13 @@ const AppError = require("./utils/AppError")
 const express = require("express")
 const routes = require("./routes")
 const uploadConfig = require("./config/upload")
+const cors = require("cors")
 migrationsRun()
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
 
