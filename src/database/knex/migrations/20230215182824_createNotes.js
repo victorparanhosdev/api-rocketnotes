5112d2da0423cda5002a93068bfacc1ca46bff93
Victor Paranhos <victor_paranhos@hotmail.com>
2023-09-13 09:41:58 -0300

    fim

--- src/database/knex/migrations/20230215182824_createNotes.js
+++ src/database/knex/migrations/20230215182824_createNotes.js
@@ -1,12 +1,13 @@
-
+const moment = require('moment-timezone');
+const brazilTimezone = 'America/Sao_Paulo';
 exports.up = knex => knex.schema.createTable("notes", table =>  {
   table.increments("id");
   table.text("title");
   table.text("description");
   table.integer("user_id").references("id").inTable("users");
 
-  table.timestamp("created_at").default(knex.fn.now());
-  table.timestamp("updated_at").default(knex.fn.now());
+  table.string('created_at').defaultTo(moment.tz(brazilTimezone).format('DD/MM/YYYY HH:mm:ss'));
+  table.string('updated_at').defaultTo(moment.tz(brazilTimezone).format('DD/MM/YYYY HH:mm:ss'));
   
 });
 
